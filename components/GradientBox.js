1c876f1d2e112e2328b44dab23a3658c357ce807
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-04-25 19:01:35 +0200

    Menu was implemented

--- components/GradientBox.js
+++ components/GradientBox.js
@@ -1,6 +1,6 @@
 const GradientBox = () => {
   return (
-    <div className="w-full h-1.5 bg-gradient-to-r from-indigo-600 to-rose-500" />
+    <div className="w-full h-1.5 bg-gradient-to-r from-blueberry to-redSalsa" />
   );
 };
 
--- components/Menu.js
+++ components/Menu.js
@@ -1,5 +1,36 @@
+import HorizontalLogotype from "../images/HorizontalLogotype";
+import MagnifyingGlass from "../images/MagnifyingGlass";
+import en from "../lozalizations/en.json";
+
+const MenuItem = ({ title, marginLeft }) => {
+  return (
+    <a
+      href=""
+      className={`${"text-white text-base font-extrabold"} ${marginLeft}`}
+    >
+      {title}
+    </a>
+  );
+};
+
 const Menu = () => {
-  return <p>Menu</p>;
+  return (
+    <div className="w-full h-24 bg-black items-center flex px-10 py-6">
+      <div className="w-48">
+        <HorizontalLogotype />
+      </div>
+      <div className="ml-56 bg-gray-dark h-11 rounded-lg px-5 py-3.5 flex w-screen">
+        <MagnifyingGlass />
+        <input
+          className="text-white 5 outline-0 bg-gray-dark ml-5 text-sm text-gray w-full"
+          placeholder={en.searchPlaceholder}
+        />
+      </div>
+      <MenuItem title={en.menuItem} marginLeft="ml-10" />
+      <MenuItem title={en.menuItem} marginLeft="ml-14" />
+      <MenuItem title={en.menuItem} marginLeft="ml-9" />
+    </div>
+  );
 };
 
 export default Menu;
