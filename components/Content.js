aa4acb505bbac7c29db734d2b2f8f6ad8163b5bf
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-04-25 12:07:53 +0200

    Localizations file was added

--- components/Content.js
+++ components/Content.js
@@ -1,5 +1,6 @@
 import Box from "./Box";
 import ButtonsRow from "./ButtonsRow";
+import en from "../lozalizations/en.json";
 
 const Content = () => {
   return (
@@ -7,35 +8,14 @@ const Content = () => {
       <ButtonsRow />
       <div className="grid grid-cols-2 gap-4 ">
         <Box>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
-            mi ut luctus blandit. Maecenas rutrum mollis sem ac varius. Mauris
-            vestibulum tortor eget sem egestas, sit amet pharetra erat
-            tincidunt. Donec et rhoncus felis, at efficitur dui. Sed sit amet
-            augue volutpat turpis euismod luctus. Phasellus fermentum risus a
-            massa elementum, eu elementum lacus laoreet.
-          </div>
+          <div>{en.contentTab}</div>
         </Box>
         <Box>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
-            mi ut luctus blandit. Maecenas rutrum mollis sem ac varius. Mauris
-            vestibulum tortor eget sem egestas, sit amet pharetra erat
-            tincidunt. Donec et rhoncus felis, at efficitur dui. Sed sit amet
-            augue volutpat turpis euismod luctus. Phasellus fermentum risus a
-            massa elementum, eu elementum lacus laoreet.
-          </div>
+          <div>{en.contentTab}</div>
         </Box>
         <div className="col-span-1" />
         <Box>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
-            mi ut luctus blandit. Maecenas rutrum mollis sem ac varius. Mauris
-            vestibulum tortor eget sem egestas, sit amet pharetra erat
-            tincidunt. Donec et rhoncus felis, at efficitur dui. Sed sit amet
-            augue volutpat turpis euismod luctus. Phasellus fermentum risus a
-            massa elementum, eu elementum lacus laoreet.
-          </div>
+          <div>{en.contentTab}</div>
         </Box>
       </div>
     </div>
