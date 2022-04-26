1d6f95f9ec54d80a3052d8974bd7caa3b35f76a7
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-04-26 13:01:39 +0200

    Main Header was implemented

--- components/MainBox.js
+++
@@ -1,9 +0,0 @@
-const MainBox = () => {
-  return (
-    <div>
-      <p>MainBox</p>
-    </div>
-  );
-};
-
-export default MainBox;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Content from "../components/Content";
 import Menu from "../components/Menu";
 import GradientBox from "../components/GradientBox";
-import MainBox from "../components/MainBox";
 import Footer from "../components/Footer";
+import Image from "next/image";
+
+const Header = require("../images/Header.png");
+const HeaderCircle = require("../images/HeaderCircle.png");
 
 export default function Home() {
   return (
@@ -18,9 +20,14 @@ export default function Home() {
       <Menu />
       <GradientBox />
       <main
-        className={`${styles.main} bg-gradient-to-b from-[#2E2D66] via-[#3A273F] to-[#181818]`}
+        className={`${styles.main} bg-gradient-to-b from-[#2E2D66] via-[#3A273F] to-[#181818] px-[10px]`}
       >
-        <MainBox />
+        <div className="w-full pt-[41px]">
+          <Image src={Header} alt="Main Header" />
+        </div>
+        <div className=" pb-[13px] translate-y-[-50%] w-[149px] h-[149px]">
+          <Image src={HeaderCircle} alt="HeaderCircle" />
+        </div>
         <Content />
       </main>
       <Footer />
